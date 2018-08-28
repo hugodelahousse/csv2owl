a1abc122bf83981d43260c00c25d6497e5ed5fd7
Hugo Delahousse <hugo.delahousse@gmail.com>
2018-08-28 09:58:42 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(name='csv2owl',
+      version='1.0',
+      description='A tool to generate owl vocabularies for csv properties and classes files',
+      author='Hugo Delahousse',
+      author_email='hugo.delahousse@gmail.com',
+      url='https://www.github.com/hugodelahousse/owl2csv/',
+      license='MIT',
+      scripts=['csv2owl'],
+      install_requires=[
+            'click',
+            'rdflib',
+            'rdflib-jsonld',
+      ]
+)
